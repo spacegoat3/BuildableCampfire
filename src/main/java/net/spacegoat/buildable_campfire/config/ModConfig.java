bce33fb3620704540c8955bd7c017021ebfdb238
spacegoat3 <70437195+spacegoat3@users.noreply.github.com>
2022-03-15 00:11:14 -0700

    Changed and added some config options and categories.

--- src/main/java/net/spacegoat/buildable_campfire/config/ModConfig.java
+++ src/main/java/net/spacegoat/buildable_campfire/config/ModConfig.java
@@ -7,9 +7,9 @@ import me.shedaniel.autoconfig.annotation.ConfigEntry;
 import me.shedaniel.autoconfig.serializer.JanksonConfigSerializer;
 import me.shedaniel.cloth.clothconfig.shadowed.blue.endless.jankson.Comment;
 import net.minecraft.util.Formatting;
-import net.spacegoat.buildable_campfire.BuildableCampfireMod;
+import net.spacegoat.buildable_campfire.BuildableCampfire;
 
-@Config(name = BuildableCampfireMod.MOD_ID)
+@Config(name = BuildableCampfire.MOD_ID)
 @Config.Gui.Background("minecraft:textures/block/oak_log.png")
 public class ModConfig implements ConfigData {
 
@@ -51,40 +51,59 @@ public class ModConfig implements ConfigData {
         public String tooltip = "";
         @Comment("The color of the tooltip text.")
         public Formatting tooltipColor = Formatting.GRAY;
-        @Comment("Changes the Campfire Log's name to Firewood. (User Suggestion)")
-        public boolean changeCampfireLogName = false;
         @Comment("The amount of light Campfire Log gives off to its surroundings.")
         public int campfireLogLuminance = 0;
     }
     @ConfigEntry.Gui.TransitiveObject
-    @ConfigEntry.Category("logs")
-    public Logs logs = new Logs();
-    public static class Logs {
-        public boolean oakLog = true;
-    }
-
-    @ConfigEntry.Gui.TransitiveObject
     @ConfigEntry.Category("campfire_block")
-    public CampfireBlock CampfireBlock = new CampfireBlock();
-    public static class CampfireBlock {
+    public CampfireBlockConfig CampfireBlock = new CampfireBlockConfig();
+    public static class CampfireBlockConfig {
+        @Comment("Minecraft - Orange/Yellow Smoked Campfire")
         public boolean enableBuildableCampfire = true;
         @Comment("Should your Campfire be lit when you build it?")
         public boolean campfireIsLitWhenBuild = false;
         @Comment("Plays a sound effect when you right-click a finished Campfire Template with a coal or charcoal.")
         public boolean playSoundEffect = true;
         @Comment("The amount of Coal/Charcoal you will need to build a Campfire.")
-        public int howMuchCoalBuildingACampfireTakes = 1;
+        public int howMuchCoalBuildingACampfireCosts = 1;
     }
     @ConfigEntry.Gui.TransitiveObject
     @ConfigEntry.Category("soul_campfire_block")
-    public SoulCampfireBlock SoulCampfireBlock = new SoulCampfireBlock();
-    public static class SoulCampfireBlock {
+    public SoulCampfireBlockConfig SoulCampfireBlock = new SoulCampfireBlockConfig();
+    public static class SoulCampfireBlockConfig {
+        @Comment("Minecraft - Blue Smoked Campfire")
         public boolean enableBuildableSoulCampfire = true;
         @Comment("Should your Soul Campfire be lit when you build it?")
         public boolean soulCampfireIsLitWhenBuild = false;
         @Comment("Plays a sound effect when you right-click a finished Campfire Template with Soul Sand.")
         public boolean playSoundEffect = true;
         @Comment("The amount of Soul Sand you will need to build a Soul Campfire.")
-        public int howMuchSoulSandBuildingASoulCampfireTakes = 1;
+        public int howMuchSoulSandBuildingASoulCampfireCosts = 1;
+    }
+    @ConfigEntry.Gui.TransitiveObject
+    @ConfigEntry.Category("boric_campfire_block")
+    public BoricCampfireBlockConfig BoricCampfire = new BoricCampfireBlockConfig();
+    public static class BoricCampfireBlockConfig {
+        @Comment("Oh The Biomes You'll Go - Green Smoked Campfire")
+        public boolean enableBuildableBoricCampfire = true;
+        @Comment("Should your Boric Campfire be lit when you build it?")
+        public boolean boricCampfireIsLitWhenBuild = false;
+        @Comment("Plays a sound effect when you right-click a finished Campfire Template with Brim Powder.")
+        public boolean playSoundEffect = true;
+        @Comment("The amount of Brim Powder you will need to build a Boric Campfire.")
+        public int howMuchBrimPowderBuildingABoricCampfireCosts = 1;
+    }
+    @ConfigEntry.Gui.TransitiveObject
+    @ConfigEntry.Category("cryptic_campfire_block")
+    public CrypticCampfireBlockConfig CrypticCampfire = new CrypticCampfireBlockConfig();
+    public static class CrypticCampfireBlockConfig {
+        @Comment("Oh The Biomes You'll Go - Green Smoked Campfire")
+        public boolean enableBuildableCrypticCampfire = true;
+        @Comment("Should your Boric Campfire be lit when you build it?")
+        public boolean crypticCampfireIsLitWhenBuild = false;
+        @Comment("Plays a sound effect when you right-click a finished Campfire Template with Brim Powder.")
+        public boolean playSoundEffect = true;
+        @Comment("The amount of Brim Powder you will need to build a Boric Campfire.")
+        public int howMuchCrypticMagmaBlockBuildingACrypticCampfireCosts = 1;
     }
 }
