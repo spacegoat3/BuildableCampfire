64cbb8b5ae17322a682aec711fdb44f785aacf54
spacegoat3 <70437195+spacegoat3@users.noreply.github.com>
2022-03-15 00:14:26 -0700

    Added an init class for blocks.

--- src/main/java/net/spacegoat/buildable_campfire/CampfireLogs.java
+++ src/main/java/net/spacegoat/buildable_campfire/CampfireLogs.java
@@ -1,36 +1,37 @@
 package net.spacegoat.buildable_campfire;
 
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
 import net.minecraft.block.Material;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.ItemGroup;
 import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
-import net.spacegoat.buildable_campfire.blocks.CampfireLogBlock;
-import net.spacegoat.buildable_campfire.blocks.VanillaCampfireLogBlock;
 import net.spacegoat.buildable_campfire.config.ModConfig;
 
 public class CampfireLogs {
-    private static FabricBlockSettings CAMPFIRE_LOG_SETTINGS(Block mapColor){
-        return FabricBlockSettings.of(Material.WOOD).strength(Blocks.CAMPFIRE.getHardness())
-                .sounds(BlockSoundGroup.WOOD).mapColor(mapColor.getDefaultMapColor()).luminance(ModConfig.getConfig().Gameplay.campfireLogLuminance);
-    }
+    private static final FabricBlockSettings CAMPFIRE_LOG_SETTINGS = FabricBlockSettings.of(Material.WOOD).strength(Blocks.CAMPFIRE.getHardness())
+            .sounds(BlockSoundGroup.WOOD).luminance(ModConfig.getConfig().Gameplay.campfireLogLuminance);
 
-    public static final VanillaCampfireLogBlock CAMPFIRE_LOG = createCampfireLog("campfire_log", Blocks.OAK_LOG);
+    public static final CampfireLogBlock CAMPFIRE_LOG = createCampfireLog
+            ("campfire", Blocks.CAMPFIRE, Blocks.SOUL_CAMPFIRE, CAMPFIRE_LOG_SETTINGS);
 
-    private static VanillaCampfireLogBlock createCampfireLog(String id, Block mapColor){
-        return new VanillaCampfireLogBlock(id, FabricBlockSettings.of(Material.WOOD).strength(Blocks.CAMPFIRE.getHardness())
-                .sounds(BlockSoundGroup.WOOD).mapColor(mapColor.getDefaultMapColor()).luminance(ModConfig.getConfig().Gameplay.campfireLogLuminance));
+    private static CampfireLogBlock createCampfireLog(String id, Block campfire, Block soulCampfire, FabricBlockSettings settings){
+        return new CampfireLogBlock(id, campfire, soulCampfire, FabricBlockSettings.of(Material.WOOD).strength(Blocks.CAMPFIRE.getHardness())
+                .sounds(BlockSoundGroup.WOOD).luminance(ModConfig.getConfig().Gameplay.campfireLogLuminance));
     }
 
     public static void registerCampfireLogs(){
-        if (ModConfig.getConfig().logs.oakLog){
-            register(CAMPFIRE_LOG);
-        }
+        register(CAMPFIRE_LOG);
     }
 
     private static void register(CampfireLogBlock block){
-        Registry.register(Registry.BLOCK, new Identifier(BuildableCampfireMod.MOD_ID, block.getId()), block);
+        Identifier identifier = new Identifier(BuildableCampfire.MOD_ID, block.getId());
+        Registry.register(Registry.BLOCK, identifier, block);
+        Registry.register(Registry.ITEM, identifier, new BlockItem(block, new FabricItemSettings()
+                .group(ItemGroup.DECORATIONS)));
     }
 }
